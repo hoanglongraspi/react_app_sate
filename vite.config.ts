bb160cce6ed9787c56ec77b78bbdd1819867d60d
Long Cao <33237818+hoanglongraspi@users.noreply.github.com>
2025-06-12 02:46:23 -0400

    check

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import { fileURLToPath, URL } from 'node:url'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(fileURLToPath(new URL('./src', import.meta.url))),
+      "@": path.resolve("src"),
     },
   }
 })
